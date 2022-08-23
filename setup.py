e3525e72b5684eafd0091a8ebc84fd31e4b3570d
chaianun damrongrat <chaianun.damrongrat@nectec.or.th>
2022-08-23 11:05:02 +0700

    o [util.py]   - remain only the core module o [README]   - more description o [setup]   - clean content

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-#import pathlib
 from setuptools import setup
 from setuptools import find_packages
 
-#The directory containing this file
-#HERE = pathlib.Path(__file__).parent
-#The text of the README file
-#README = (HERE / "README.md").read_text()
 README = ''.join([line for line in open("README.md").readlines()])
-#This call to setup() does all the work
 setup(
     name="muuusiiik",
-    version="0.0.10",
+    version="0.0.11",
     author="@muuusiiik",
     author_email="muuusiiikd@gmail.com",
     description="simple utilities",
     long_description=README,
     long_description_content_type="text/markdown",
-    #url="https://github.com/c-tawayip/longbug_util",
     url="https://github.com/muuusiiik/utility",
     python_requires=">=3.7",
     license="MIT",
@@ -34,7 +27,5 @@ setup(
 
      # DEPENDENCIES PACKAGES
      install_requires=["dill", "PyYaml"],
-     #setup_requires=["logging", "time", "pathlib", "dill", "yaml"],
-     #install_requires=[],
  )
 
