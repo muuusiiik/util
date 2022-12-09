f173a20678399fcc1742569a54e59ff2d57f65aa
chaianun damrongrat <chaianun.damrongrat@nectec.or.th>
2022-12-09 13:26:49 +0700

    o [setup, __init__] update content for supporting different packages but use the same namespace {muuusiiik}

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 README = ''.join([line for line in open("README.md").readlines()])
 setup(
     name="muuusiiik",
-    version="0.0.21",
+    version="0.0.22",
     author="@muuusiiik",
     author_email="muuusiiikd@gmail.com",
     description="simple utilities",
@@ -20,6 +20,7 @@ setup(
      ],
      #packages=["muuusiiik"],
      packages=find_packages("."),
+     namespace_package=['muuusiiik'],
 
      # DEAL WITH DATA
      #package_data={"vocab": ["vocab/*"]}
