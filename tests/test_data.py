46ec7d50ee7f2e49f504ecee0414385436080caf
chaianun damrongrat <chaianun.damrongrat@nectec.or.th>
2022-12-16 08:01:57 +0700

    o [util-data] done with test on removing an exsiting folder

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,6 +5,26 @@ from   pytest import raises
 
 
 # -----------------------------
+# MOCK :: CREATE & REMOVE FILE
+# -----------------------------
+class Mock:
+    def make_sure_there_is_a_file(file_name:str):
+        fd, fn = msk.data.path_split(file_name)
+        msk.data.make_path(fd)
+        msk.data.save('', file_name)
+
+    def make_sure_the_folder_removed(path:str):
+        msk.data.rm(path)
+
+    def make_a_new_folder_with_3_files_1_folder_inside(folder, fname):
+        # mock a new folder with 3 files and 1 folder inside
+        file_name = f'{folder}/{fname}'
+        for i in range(3): Mock.make_sure_there_is_a_file(f'{file_name}_{i:02}.txt')
+        msk.data.make_path(f'{folder}/not_exist_folder', pathtype='folder')
+
+
+
+# -----------------------------
 # FILE PATH SPLIT 
 # -----------------------------
 def test_path_split_on_folder_path():
@@ -144,43 +164,25 @@ def test_raise_type_error_for_incorrect_path_type():
 
 
 # -----------------------------
-# MOCK :: CREATE & REMOVE FILE
-# -----------------------------
-def _make_sure_there_is_a_file(file_name:str):
-    fd, fn = msk.data.path_split(file_name)
-    msk.data.make_path(fd)
-    msk.data.save('', file_name)
-
-def _make_sure_the_folder_removed(path:str):
-    msk.data.rm(path)
-
-def _make_a_new_folder_with_3_files_1_folder_inside(folder, fname):
-    # mock a new folder with 3 files and 1 folder inside
-    file_name = f'{folder}/{fname}'
-    for i in range(3): _make_sure_there_is_a_file(f'{file_name}_{i:02}.txt')
-    msk.data.make_path(f'{folder}/temp_folder', pathtype='folder')
-
-
-# -----------------------------
 # LS A SPECIFIC PATH
 # -----------------------------
 def test_list_contents_in_a_given_folder_in_list_format():
     # mock a new folder with 3 files and 1 folder inside
     file_name = 'tests/_data/some_file'
     fd, fn    = msk.data.path_split(file_name)
-    _make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
+    Mock.make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
 
     # check ontents in given folder
     result    = msk.data.ls(fd)
     assert len(result) == 4
-    assert ('temp_folder/' in result)     == True
-    assert ('some_file_00.txt' in result) == True
-    assert ('some_file_01.txt' in result) == True
-    assert ('some_file_02.txt' in result) == True
-    assert ('some_file_03.txt' in result) == False
+    assert ('not_exist_folder/' in result) == True
+    assert ('some_file_00.txt'  in result) == True
+    assert ('some_file_01.txt'  in result) == True
+    assert ('some_file_02.txt'  in result) == True
+    assert ('some_file_03.txt'  in result) == False
 
     # remove the folder
-    _make_sure_the_folder_removed(fd)
+    Mock.make_sure_the_folder_removed(fd)
     assert msk.data.exist(fd) == False
 
 
@@ -188,21 +190,21 @@ def test_list_contents_in_a_given_folder_in_dict_format():
     # mock a new folder with 3 files and 1 folder inside
     file_name = 'tests/_data/some_file'
     fd, fn    = msk.data.path_split(file_name)
-    _make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
+    Mock.make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
 
     # check ontents in given folder
     result    = msk.data.ls(fd, fmt='dict')
     assert len(result) == 2
     assert len(result['folder']) == 1
     assert len(result['file'  ]) == 3
-    assert ('temp_folder'      in result['folder']) == True
+    assert ('not_exist_folder' in result['folder']) == True
     assert ('some_file_00.txt' in result['file'  ]) == True
     assert ('some_file_01.txt' in result['file'  ]) == True
     assert ('some_file_02.txt' in result['file'  ]) == True
     assert ('some_file_03.txt' in result['file'  ]) == False
 
     # remove the folder
-    _make_sure_the_folder_removed(fd)
+    Mock.make_sure_the_folder_removed(fd)
     assert msk.data.exist(fd) == False
 
 
@@ -229,33 +231,53 @@ def test_list_conents_in_a_path_the_same_as_file_name():
     with raises(NotADirectoryError):
         result = msk.data.ls(path)
 
+
 # -----------------------------
 # REMOVE FILE & FOLDER
 # -----------------------------
-def _test_remove_a_file():
-    file_name = 'tests/_data/some_file.txt'
-    # make sure there is a certain file in folder
-    _make_sure_there_is_a_file(file_name)
-    # do remove a file
-    result    = msk.data.rm(file_name)
-    # get folder and file name to fd and fl, respectively
-    fd, fl    = msk.data.path_split(file_name)
-    # get contents in folder
-    ls_list   = msk.data.ls(fd)
+def test_remove_a_file():
+    # mock content 
+    file_name = 'tests/_data/some_file'
+    fd, fn    = msk.data.path_split(file_name)
+    Mock.make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
 
-    assert result == True
-    assert file_name not in ls_list
+    # the mocked contents have 3 files and 1 folder
+    ls_list   = msk.data.ls(fd)
+    assert len(ls_list) == 4
+
+    # remove file 01
+    f         = f'{file_name}_01.txt'
+    result    = msk.data.rm(f)
+    ls_list   = msk.data.ls(fd)
+    assert result         == True
+    assert len(ls_list)   == 3
+    assert (f in ls_list) == False
+
+    # reset to zero
+    Mock.make_sure_the_folder_removed(fd)
+    assert msk.data.exist(fd) == False
 
 
 def test_remove_a_folder():
+    # mock content 
+    file_name = 'tests/_data/some_file'
+    fd, fn    = msk.data.path_split(file_name)
+    Mock.make_a_new_folder_with_3_files_1_folder_inside(fd, fn)
+
+    # remove tests/_data/
+    result    = msk.data.rm(fd)
+    assert result == True
+
+    # folder tests/_data/ does not exist anymore in tests/
+    ls_list   = msk.data.ls('tests')
+    assert (fd in ls_list) == False
+
+
+def _test_remove_a_non_existing_file():
     ...
 
 
-def test_remove_a_non_existing_file():
-    ...
-
-
-def test_remove_a_non_existing_folder():
+def _test_remove_a_non_existing_folder():
     ...
 
 
